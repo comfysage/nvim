ee4db2be1a97e82ba2ded9570da84da55fcd3d1a
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-11-30 20:26:54 +0100

    add(config.neovide): adjust neovide looks

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,11 +1,11 @@
-vim.o.guifont = "JetBrainsMono NF:h9"
+vim.o.guifont = "Maple Mono:h9"
 vim.opt.linespace = 1
 vim.g.neovide_padding_top = 1
 vim.g.neovide_padding_bottom = 1
 vim.g.neovide_padding_left = 1
 vim.g.neovide_padding_right = 1
 
-vim.g.neovide_transparency = 1.0
+vim.g.neovide_transparency = 0.9
 vim.g.transparency = 1
 
 toggle_transparent_background(false)
