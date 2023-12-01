8675009e6913380bdbef6dcfd57f874b4b30d5df
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-01 15:26:39 +0100

    rework(core): move core to sentinel.nvim plugin; bootstrap sentinel.nvim

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
+local rootpath = vim.fn.stdpath("data") .. "/core"
+local sentinelpath = rootpath .. "/sentinel"
+vim.opt.rtp:prepend(sentinelpath)
+
+local ok, core = pcall(require, 'core')
+if not ok then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/crispybaccoon/sentinel.nvim.git",
+    sentinelpath,
+  })
+end
 
 require 'config'
