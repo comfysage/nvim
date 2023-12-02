5bf75799b1b2b45c5ea7694318ecdaf5c998b762
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-02 12:39:37 +0100

    feat(plugins.ui.fidget): add `fidget.nvim` plugin

---
+++ lua/plugins/ui/notify.lua
@@ -0,0 +1,35 @@
+return {
+  'j-hui/fidget.nvim',
+  opts = {
+    notification = {
+      override_vim_notify = true,
+      filter = vim.log.levels.DEBUG,
+      configs = {
+        default = {
+          name = "Notifications",
+          icon = "",
+          ttl = 4,
+          group_style = "Title",
+          icon_style = "Special",
+          annote_style = "Question",
+          debug_style = "Comment",
+          info_style = "Question",
+          warn_style = "WarningMsg",
+          error_style = "ErrorMsg",
+          debug_annote = "DEBUG",
+          info_annote = "INFO",
+          warn_annote = "WARN",
+          error_annote = "ERROR",
+        },
+      },
+      window = {
+        winblend = 0,
+      },
+    },
+  },
+  config = function (_, opts)
+    require 'fidget'.setup (opts)
+
+    core.config.log_level = vim.log.levels.DEBUG
+  end
+}
