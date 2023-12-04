8d09aad2bebb8631a02bda3aee79838335652abf
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-04 12:12:27 +0100

    add(config): use `config.highlights` for `core.highlights` opts

--- lua/config/highlights.lua
+++ lua/config/highlights.lua
@@ -31,3 +31,32 @@ vim.api.nvim_create_user_command('SynStack', function()
 end, {})
 
 vim.keymap.set("n", "<leader>sp", ":call SynStack()<CR>")
+
+return {
+  fix = function()
+    vim.cmd [[ hi clear SpellCap ]]
+
+    if vim.g.neovide then
+      local alpha = function()
+        return string.format("%x", math.floor(255 * vim.g.transparency or 0.0))
+      end
+      local bg_color = '#' .. vim.api.nvim_get_hl(0, { name = 'Normal' }).bg
+
+      vim.g.neovide_background_color = bg_color .. alpha()
+    end
+
+    -- notes
+
+    local groups = {
+      fixme   = { "Fixme", vim.api.nvim_get_hl(0, { name = "DiagnosticWarn" }) },
+      todo    = { "Todo", vim.api.nvim_get_hl(0, { name = "DiagnosticInfo" }) },
+      note    = { "Note", vim.api.nvim_get_hl(0, { name = "DiagnosticHint" }) },
+      success = { "Success", vim.api.nvim_get_hl(0, { name = "DiagnosticOk" }) },
+      failure = { "Failure", vim.api.nvim_get_hl(0, { name = "DiagnosticError" }) },
+    }
+    for _, v in pairs(groups) do
+      vim.api.nvim_set_hl(0, v[1], { fg = v[2].fg })
+      vim.api.nvim_set_hl(0, v[1] .. 'Note', { fg = v[2].fg, reverse = true })
+    end
+  end
+}
