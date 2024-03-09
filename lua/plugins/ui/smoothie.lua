12ad518edde4cb3592115ec68674e1c905b83c28
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-09 14:22:44 +0100

    rework(plugins.ui.smoothie): replace `vim-smoothie` with `neoscroll.nvim`

--- lua/plugins/ui/smoothie.lua
+++ lua/plugins/ui/smoothie.lua
@@ -1,9 +1,40 @@
 return {
-  Spec {
-    'psliwka/vim-smoothie',
+  {
+    'karb94/neoscroll.nvim',
+    event = 'UIEnter',
     config = function()
-      vim.g.smoothie_enabled = true
-      vim.g.smoothie_no_default_mappings = true
+      require('neoscroll').setup {
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = {},
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,       -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,             -- Function to run after the scrolling animation ends
+        performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+      }
+      local t = {}
+      -- Syntax: t[keys] = {function, {function arguments}}
+      t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '250' } }
+      t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '250' } }
+      t['<C-b>'] =
+      { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '450' } }
+      t['<C-f>'] =
+      { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450' } }
+      t['zt'] = { 'zt', { '250' } }
+      t['zz'] = { 'zz', { '250' } }
+      t['zb'] = { 'zb', { '250' } }
+
+      require('neoscroll.config').set_mappings(t)
     end,
   },
+  -- Spec {
+  --   'psliwka/vim-smoothie',
+  --   config = function()
+  --     vim.g.smoothie_enabled = true
+  --     vim.g.smoothie_no_default_mappings = true
+  --   end,
+  -- },
 }
