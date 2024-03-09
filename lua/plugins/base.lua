aafc7729b73e5f040963dde6bb6e20eadd066291
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-09 14:14:26 +0100

    feat(plugins.base): lazy load `vim-vinegar`

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -2,7 +2,7 @@ return {
   { 'mattn/emmet-vim', ft = { 'css', 'scss', 'html', 'tsx' } },
   { 'tpope/vim-fugitive', enabled = false },
   { 'sheerun/vim-polyglot', enabled = false },
-  'tpope/vim-vinegar',
+  { 'tpope/vim-vinegar', event = 'BufEnter' },
   'nvim-lua/plenary.nvim', -- utils
   'nvim-lua/popup.nvim',
 }
