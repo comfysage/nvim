17087cff537eb9367af4a8c18dbd2d058bc559f2
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-08 12:31:17 +0200

    refactor(plugins.zk): move `zk` to separate module

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -60,35 +60,6 @@ return {
   },
 
   {
-    'mickael-menu/zk-nvim',
-    ft = 'markdown',
-    opts = {
-      -- can be "telescope", "fzf" or "select" (`vim.ui.select`)
-      -- it's recommended to use "telescope" or "fzf"
-      picker = 'telescope',
-
-      lsp = {
-        -- `config` is passed to `vim.lsp.start_client(config)`
-        config = {
-          cmd = { 'zk', 'lsp' },
-          name = 'zk_lsp',
-          -- on_attach = ...
-          -- etc, see `:h vim.lsp.start_client()`
-        },
-
-        -- automatically attach buffers in a zk notebook that match the given filetypes
-        auto_attach = {
-          enabled = true,
-          filetypes = { 'markdown' },
-        },
-      },
-    },
-    config = function(_, opts)
-      require('zk').setup(opts)
-    end,
-  },
-
-  {
     'comfysage/sayama.nvim',
     event = 'BufAdd',
     config = function()
