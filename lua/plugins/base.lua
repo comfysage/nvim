221a6caf30fdbb38ad63bf519215fce50aa2358e
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-08 12:19:32 +0200

    fix: remove unneeded base plugins

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,8 +1,4 @@
 return {
   { 'mattn/emmet-vim', ft = { 'css', 'scss', 'html', 'tsx' } },
-  { 'tpope/vim-fugitive', enabled = false },
-  { 'sheerun/vim-polyglot', enabled = false },
-  { 'tpope/vim-vinegar', event = 'BufEnter' },
   'nvim-lua/plenary.nvim', -- utils
-  'nvim-lua/popup.nvim',
 }
